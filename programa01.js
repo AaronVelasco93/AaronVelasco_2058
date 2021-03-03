e2247420c520bf9186b0a048d87a7561dcac47ae
AaronVelasco93 <aaronvelasco1993@outlook.com>
2021-03-02 18:30:13 -0600

    mi hola mundo en js

---
+++ programa01.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+ <html>
+  <body>
+    <button onclick ="function1()"> preciona</button>
+      <p id= "element1">Bienbenido</p>
+    
+    <script>
+        function function1(){
+          document.getElemtByID("element1").innerHTML ="Hola mundo";
+      }
+
+    </script>
+  </body>
+ </html>
+  
